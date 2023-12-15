e60981341b8e253742eb0663560c734b0e61d345
Micah Newsum <micah.newsum@glidewelldental.com>
2023-12-14 22:25:51 -0800

    feat: create SimplePizzaFactory class

---
+++ factory/CheesePizza.java
@@ -0,0 +1,30 @@
+package factory;
+
+public class CheesePizza implements Pizza {
+    public CheesePizza() {
+    }
+
+    @Override
+    public void prepare() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'prepare'");
+    }
+
+    @Override
+    public void bake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'bake'");
+    }
+
+    @Override
+    public void cut() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cut'");
+    }
+
+    @Override
+    public void box() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'box'");
+    }
+}
---
+++ factory/SimplePizzaFactory.java
@@ -0,0 +1,13 @@
+package factory;
+
+public class SimplePizzaFactory {
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
+
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza();
+        }
+
+        return pizza;
+    }
+}
